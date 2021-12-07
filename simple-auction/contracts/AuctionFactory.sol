9e10b7a74104979685378550d5e10e83525ff8a1
0xEval <julien.couvy@gmail.com>
2021-12-07 18:50:16 +0800

    Add function comment

--- simple-auction/contracts/AuctionFactory.sol
+++ simple-auction/contracts/AuctionFactory.sol
@@ -4,6 +4,9 @@ pragma solidity ^0.8.0;
 
 import "./Auction.sol";
 
+/** @notice Stores & deploys Auction smart contracts.
+ * Users calling the `createAuction()` gunction will be owners of deployed Auction.
+ */
 contract AuctionFactory {
 
     Auction[] public auctions;
